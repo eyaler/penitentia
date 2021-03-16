e095eb153cc838e0cc9b682888b2a860f3d4949f
eyaler <eyalgruss@gmail.com>
2021-03-16 05:06:54 +0200

    save hex outputs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # The Penitentia Manuscript Parser
 https://www.hydeandrugg.com/pages/codes/penitentia
 
-![1,1](1,1.png "1,1")
+![pentitenia 1,1.png](pentitenia 1,1.png "pentitenia 1,1.png")
--- pentitenia.py
+++ pentitenia.py
@@ -6,7 +6,8 @@ import time
 
 url = 'https://www.hydeandrugg.com/media/Content'
 images_folder = 'images'
-output_folder = 'output'
+marks_folder = 'marks'
+hex_folder = 'hex'
 y0 = 59
 x0 = 55
 y_last = 1018
@@ -16,6 +17,7 @@ dline = (111-y0)/3
 dy = 6
 dx = (118-x0)/3
 xtol = 4
+a = '01234567'
 b = '01237654'
 c = '04152637'
 d = '04512673'
@@ -45,6 +47,9 @@ d:
 1256
 '''
 
+def to_hex(byte):
+    return ('0'+hex(byte)[2:])[-2:]
+
 def to_byte(lst):
     if flip_bits:
         lst = [1-d for d in lst]
@@ -52,16 +57,22 @@ def to_byte(lst):
         lst = lst[::-1]
     byte = int(''.join(str(d) for d in lst), 2)
     if as_hex:
-        byte = ('0'+hex(byte)[2:])[-2:]
+        byte = to_hex(byte)
     return byte
 
-def to_dword(lst):
+def to_dword(lst, as_hex):
     if as_hex:
+        if type(lst[0])==int:
+            lst = [to_hex(b) for b in lst]
         return ''.join(lst)
     return tuple(lst)
 
 os.makedirs(images_folder, exist_ok=True)
-os.makedirs(output_folder, exist_ok=True)
+os.makedirs(marks_folder, exist_ok=True)
+os.makedirs(hex_folder+'/'+a, exist_ok=True)
+os.makedirs(hex_folder+'/'+b, exist_ok=True)
+os.makedirs(hex_folder+'/'+c, exist_ok=True)
+os.makedirs(hex_folder+'/'+d, exist_ok=True)
 for j in range(1,5):
     for i in range(1,5):
         filename = 'penitentia %d,%d.png'%(j,i)
@@ -93,61 +104,67 @@ for filename in os.listdir(images_folder):
     img = cv2.imread(images_folder+'/'+filename)
     id = filename.split(' ')[1].split('.')[0]
     images[id] = []
-    for row in range(12):
-        images[id].append([])
-        for col in range(12):
-            images[id][row].append([])
-            drow = ((bad_ids[id][0] if id in bad_ids else y_last) - y0) / 11
-            dcol = ((bad_ids[id][1] if id in bad_ids else x_last) - x0) / 11
-            y = int(y0 + row * drow)
-            x = int(x0 + col * dcol)
-            x1 = max(x - xtol, 0)
-            x2 = min(x + xtol + 1, img.shape[1])
-            is_red = img[y,x1:x2, 2].min() > 128
-            cnt_red[id] += is_red
-            images[id][row][col] = Quad([0]*4, [0]*4, [0]*4, [0]*4, is_red)
-            for line in range(4):
-                bytes_a = [0]*8
-                bytes_b = [0]*8
-                bytes_c = [0]*8
-                bytes_d = [0]*8
-                i = 0
-                for side in range(2):
-                    y = int(y0 + row * drow + line * dline + (2 * side - 1) * dy)
-                    for bit in range(4):
-                        x = int(x0 + col * dcol + bit * dx)
-                        x1 = max(x - xtol, 0)
-                        x2 = min(x + xtol + 1, img.shape[1])
-                        if img[y, x1:x2, 1].min() < 128:
-                            bytes_a[i] = 1
-                            bytes_b[int(b[i])] = 1
-                            bytes_c[int(c[i])] = 1
-                            bytes_d[int(d[i])] = 1
-                        img[y, x1:x2] = [is_red*255, 255*(1-is_red), 0]
-                        i += 1
-                    images[id][row][col].bytes_a[line] = to_byte(bytes_a)
-                    images[id][row][col].bytes_b[line] = to_byte(bytes_b)
-                    images[id][row][col].bytes_c[line] = to_byte(bytes_c)
-                    images[id][row][col].bytes_d[line] = to_byte(bytes_d)
-                    byte_a[images[id][row][col].bytes_a[line]] += 1
-                    byte_b[images[id][row][col].bytes_b[line]] += 1
-                    byte_c[images[id][row][col].bytes_c[line]] += 1
-                    byte_d[images[id][row][col].bytes_d[line]] += 1
-            dword_a[to_dword(images[id][row][col].bytes_a)] += 1
-            dword_b[to_dword(images[id][row][col].bytes_b)] += 1
-            dword_c[to_dword(images[id][row][col].bytes_c)] += 1
-            dword_d[to_dword(images[id][row][col].bytes_d)] += 1
-            if is_red:
-                dword_a_red[to_dword(images[id][row][col].bytes_a)] += 1
-                dword_b_red[to_dword(images[id][row][col].bytes_b)] += 1
-                dword_c_red[to_dword(images[id][row][col].bytes_c)] += 1
-                dword_d_red[to_dword(images[id][row][col].bytes_d)] += 1
-            if as_hex:
-                print(id, row, col, ''.join(images[id][row][col].bytes_a), ''.join(images[id][row][col].bytes_b),
-                  ''.join(images[id][row][col].bytes_c), ''.join(images[id][row][col].bytes_d), is_red)
-            else:
-                print(id,row,col,images[id][row][col])
-    cv2.imwrite(output_folder+'/'+filename, img)
+    with open(hex_folder+'/'+a + '/' + id+'.txt', 'w') as fa, open(hex_folder+'/'+b + '/' + id+'.txt', 'w') as fb, open(hex_folder+'/'+c + '/' + id+'.txt','w') as fc, open(hex_folder+'/'+d + '/' + id+'.txt', 'w') as fd:
+        for row in range(12):
+            images[id].append([])
+            for col in range(12):
+                images[id][row].append([])
+                drow = ((bad_ids[id][0] if id in bad_ids else y_last) - y0) / 11
+                dcol = ((bad_ids[id][1] if id in bad_ids else x_last) - x0) / 11
+                y = int(y0 + row * drow)
+                x = int(x0 + col * dcol)
+                x1 = max(x - xtol, 0)
+                x2 = min(x + xtol + 1, img.shape[1])
+                is_red = img[y,x1:x2, 2].min() > 128
+                cnt_red[id] += is_red
+                images[id][row][col] = Quad([0]*4, [0]*4, [0]*4, [0]*4, is_red)
+                for line in range(4):
+                    bytes_a = [0]*8
+                    bytes_b = [0]*8
+                    bytes_c = [0]*8
+                    bytes_d = [0]*8
+                    i = 0
+                    for side in range(2):
+                        y = int(y0 + row * drow + line * dline + (2 * side - 1) * dy)
+                        for bit in range(4):
+                            x = int(x0 + col * dcol + bit * dx)
+                            x1 = max(x - xtol, 0)
+                            x2 = min(x + xtol + 1, img.shape[1])
+                            if img[y, x1:x2, 1].min() < 128:
+                                bytes_a[i] = 1
+                                bytes_b[int(b[i])] = 1
+                                bytes_c[int(c[i])] = 1
+                                bytes_d[int(d[i])] = 1
+                            img[y, x1:x2] = [is_red*255, 255*(1-is_red), 0]
+                            i += 1
+                        images[id][row][col].bytes_a[line] = to_byte(bytes_a)
+                        images[id][row][col].bytes_b[line] = to_byte(bytes_b)
+                        images[id][row][col].bytes_c[line] = to_byte(bytes_c)
+                        images[id][row][col].bytes_d[line] = to_byte(bytes_d)
+                        byte_a[images[id][row][col].bytes_a[line]] += 1
+                        byte_b[images[id][row][col].bytes_b[line]] += 1
+                        byte_c[images[id][row][col].bytes_c[line]] += 1
+                        byte_d[images[id][row][col].bytes_d[line]] += 1
+                dword_a[to_dword(images[id][row][col].bytes_a, as_hex)] += 1
+                dword_b[to_dword(images[id][row][col].bytes_b, as_hex)] += 1
+                dword_c[to_dword(images[id][row][col].bytes_c, as_hex)] += 1
+                dword_d[to_dword(images[id][row][col].bytes_d, as_hex)] += 1
+                if is_red:
+                    dword_a_red[to_dword(images[id][row][col].bytes_a, as_hex)] += 1
+                    dword_b_red[to_dword(images[id][row][col].bytes_b, as_hex)] += 1
+                    dword_c_red[to_dword(images[id][row][col].bytes_c, as_hex)] += 1
+                    dword_d_red[to_dword(images[id][row][col].bytes_d, as_hex)] += 1
+                if as_hex:
+                    print(id, row, col, ''.join(images[id][row][col].bytes_a), ''.join(images[id][row][col].bytes_b),
+                      ''.join(images[id][row][col].bytes_c), ''.join(images[id][row][col].bytes_d), is_red)
+                else:
+                    print(id,row,col,images[id][row][col])
+                prefix = ' ' if col>0 else '\n' if row>0 else ''
+                fa.write(prefix+to_dword(images[id][row][col].bytes_a, True))
+                fb.write(prefix+to_dword(images[id][row][col].bytes_b, True))
+                fc.write(prefix+to_dword(images[id][row][col].bytes_c, True))
+                fd.write(prefix+to_dword(images[id][row][col].bytes_d, True))
+    cv2.imwrite(marks_folder+'/'+filename, img)
     if show_images:
         cv2.imshow('',img)
         cv2.setWindowTitle('', filename)
@@ -155,6 +172,7 @@ for filename in os.listdir(images_folder):
         if ch==27:
             cv2.destroyWindow('')
             show_images = False
+
 print('a:',len(byte_a),byte_a.most_common(mc_len))
 print('b:',len(byte_b),byte_b.most_common(mc_len))
 print('c:',len(byte_c),byte_c.most_common(mc_len))
